98ecf75337882aeb2a22c19f27a2e6ac8cb7e6ae
Morda-boy <34206382+Morda-boy@users.noreply.github.com>
2026-04-05 19:12:46 +0800

    Update delievery-sorokin.js

--- delievery-sorokin.js
+++ delievery-sorokin.js
@@ -244,7 +244,7 @@ $(window).load(function() {
 			});
 		}
 
-		$.getJSON('https://morda-boy.github.io/map-hongkong-2025-02.geojson', function(data) {
+		$.getJSON('https://morda-boy.github.io/gk-delievery-sorokin-04-2026.geojson', function(data) {
 			console.log(data, obj);
 			obj = data;
 			onZonesLoad(obj);
