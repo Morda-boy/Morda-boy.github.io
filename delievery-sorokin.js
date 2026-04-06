1d03fb1b27cf2244dd015d9a4d1a04d322a573a0
Morda-boy <34206382+Morda-boy@users.noreply.github.com>
2026-04-06 09:26:23 +0800

    Update delievery-sorokin.js

--- delievery-sorokin.js
+++ delievery-sorokin.js
@@ -1,253 +1,297 @@
+
 $(window).load(function() {
 	$("[name='adress']").attr('type', 'text');
 	$("[name='adress']").attr("required", "true");
 	$("[name='adress']").attr('id', 'suggestions');
-
-	$('<div class="t-text" name="dostavka_info" style="margin-top: 10px;font-style: italic;"><br></div>').insertAfter("[name='adress']");
-
-	tippy('#suggestions', {theme: 'custom-dark', content: 'Пожалуйста, укажите адрес полностью: улица, дом, квартира',});
-	const tip_address = tippy(document.querySelector('#suggestions'));
-	tip_address.setContent('Пожалуйста, укажите адрес полностью: улица, дом, квартира');
-	tip_address.setProps({
-		arrow: true,
-		animation: 'scale',
-		zIndex: 99999999,
-		theme: 'black',
-	});
-
-	ymaps.ready(init);
-	var myMap;
-	var deliveryZones;
-	var obj;
-
-	function pointInPoly(point, polygon) {
-		var x = point[0], y = point[1], inside = false;
-		for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-			var xi = polygon[i][0], yi = polygon[i][1];
-			var xj = polygon[j][0], yj = polygon[j][1];
-			var intersect = ((yi > y) !== (yj > y)) &&
-							(x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-			if (intersect) inside = !inside;
-		}
-		return inside;
-	}
-
-	function checkZoneGeoJSON(lon, lat, callback) {
-		fetch('https://morda-boy.github.io/gk138-04-04-2026.geojson')
-			.then(function(r) { return r.json(); })
-			.then(function(data) {
-				var zone = null;
-				for (var i = 0; i < data.features.length; i++) {
-					var f = data.features[i];
-					var polys = f.geometry.type === 'Polygon'
-						? [f.geometry.coordinates[0]]
-						: f.geometry.coordinates.map(function(p) { return p[0]; });
-					for (var k = 0; k < polys.length; k++) {
-						if (pointInPoly([lon, lat], polys[k])) {
-							zone = f.properties.description;
-							break;
-						}
-					}
-					if (zone) break;
-				}
-				callback(zone ? zone.trim().toLowerCase() : null);
-			});
-	}
-
-	function init() {
-		myMap = new ymaps.Map('map__0', {
-				center: [104.275385, 52.277419],
-				zoom: 12,
+	// $("[name='adress']").attr('data-msg', 'Пожалуйста, укажите адрес полностью: улица, дом, квартира');
+$('<div class="t-text" name="dostavka_info" style="margin-top: 10px;font-style: italic;"><br></div>').insertAfter("[name='adress']");
+// $("#suggestions").attr("title","Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+// $("#suggestions").attr("data-toggle","tooltip");
+tippy('#suggestions', {theme: 'custom-dark', content: 'Пожалуйста, укажите адрес полностью: улица, дом, квартира',});
+const tip_address = tippy(document.querySelector('#suggestions'));;
+tip_address.setContent('Пожалуйста, укажите адрес полностью: улица, дом, квартира');
+tip_address.setProps({
+  arrow: true,
+  animation: 'scale',
+  zIndex: 99999999,
+  theme: 'black',
+});
+console.log(tip_address);
+	
+ymaps.ready(init);
+var myMap;
+var deliveryZones;
+var obj;
+function init() {
+	myMap = new ymaps.Map('map__0', {
+			center: [104.275385, 52.277419],
+			zoom: 12,
 				autoFitToViewport: "always",
-				controls: ['geolocationControl', 'searchControl']
-			}),
-			deliveryPoint = new ymaps.GeoObject({
-				geometry: {type: 'Point'},
-				properties: {iconCaption: 'Ваш адрес'}
-			}, {
-				preset: 'islands#blackDotIconWithCaption',
-				draggable: true,
-				iconCaptionMaxWidth: '215'
-			}),
-			searchControl = myMap.controls.get('searchControl');
-		searchControl.options.set({noPlacemark: true, placeholderContent: 'Введите адрес доставки'});
-		myMap.geoObjects.add(deliveryPoint);
+			controls: ['geolocationControl', 'searchControl']
+		}),
+		deliveryPoint = new ymaps.GeoObject({
+			geometry: {type: 'Point'},
+			properties: {iconCaption: 'Ваш адрес'}
+		}, {
+			preset: 'islands#blackDotIconWithCaption',
+			draggable: true,
+			iconCaptionMaxWidth: '215'
+		}),
+		searchControl = myMap.controls.get('searchControl');
+	searchControl.options.set({noPlacemark: true, placeholderContent: 'Введите адрес доставки'});
+	myMap.geoObjects.add(deliveryPoint);
 
-		function onZonesLoad(json) {
-			deliveryZones = ymaps.geoQuery(json).addToMap(myMap);
-			deliveryZones.each(function (obj) {
-				obj.options.set({
-					fillColor: obj.properties.get('fill'),
-					fillOpacity: obj.properties.get('fill-opacity'),
-					strokeColor: obj.properties.get('stroke'),
-					strokeWidth: obj.properties.get('stroke-width'),
-					strokeOpacity: obj.properties.get('stroke-opacity')
-				});
-				obj.properties.set('balloonContent', obj.properties.get('description'));
+	function onZonesLoad(json) {
+		// Добавляем зоны на карту.
+		deliveryZones = ymaps.geoQuery(json).addToMap(myMap);
+		// Задаём цвет и контент балунов полигонов.
+		deliveryZones.each(function (obj) {
+			obj.options.set({
+				fillColor: obj.properties.get('fill'),
+				fillOpacity: obj.properties.get('fill-opacity'),
+				strokeColor: obj.properties.get('stroke'),
+				strokeWidth: obj.properties.get('stroke-width'),
+				strokeOpacity: obj.properties.get('stroke-opacity')
 			});
+			obj.properties.set('balloonContent', obj.properties.get('description'));
+		});
 
-			// При смене варианта доставки — разблокируем кнопку
-			$('input[name="deliveryvar"]').on('change', function() {
-				if ($(this).val() !== 'Доставка (до подъезда)') {
-					$('button[type="submit"]').prop('disabled', false).css('opacity', '1');
-					$("[name='dostavka_info']").html('');
-				}
-			});
+		// Проверим попадание результата поиска в одну из зон доставки.
+		searchControl.events.add('resultshow', function (e) {
+			console.log("еее  бой");
+			highlightResult(searchControl.getResultsArray()[e.get('index')]);
+		});
 
-			searchControl.events.add('resultshow', function (e) {
-				highlightResult(searchControl.getResultsArray()[e.get('index')]);
-			});
+		// Проверим попадание метки геолокации в одну из зон доставки.
+		myMap.controls.get('geolocationControl').events.add('locationchange', function (e) {
+			highlightResult(e.get('geoObjects').get(0));
+		});
 
-			myMap.controls.get('geolocationControl').events.add('locationchange', function (e) {
-				highlightResult(e.get('geoObjects').get(0));
-			});
+		// При перемещении метки сбрасываем подпись, содержимое балуна и перекрашиваем метку.
+		deliveryPoint.events.add('dragstart', function () {
+			deliveryPoint.properties.set({iconCaption: '', balloonContent: ''});
+			deliveryPoint.options.set('iconColor', 'black');
+		});
 
-			deliveryPoint.events.add('dragstart', function () {
-				deliveryPoint.properties.set({iconCaption: '', balloonContent: ''});
-				deliveryPoint.options.set('iconColor', 'black');
-			});
+		// По окончании перемещения метки вызываем функцию выделения зоны доставки.
+		deliveryPoint.events.add('dragend', function () {
+			highlightResult(deliveryPoint);
+		});
 
-			deliveryPoint.events.add('dragend', function () {
-				highlightResult(deliveryPoint);
-			});
+		function highlightResult(obj) {
+			console.log(obj);
+			console.log(obj.geometry.getCoordinates());
+			$("#suggestions").val($(".ymaps-2-1-77-searchbox-input__input").val());
+			$("[name='dostavka_info'").html(polygon.properties.get('description'));
 
-			function highlightResult(obj) {
-				var coords = obj.geometry.getCoordinates();
-				var polygon = deliveryZones.searchContaining(coords).get(0);
-
-				if (polygon) {
-					deliveryZones.setOptions('fillOpacity', 0.4);
-					polygon.options.set('fillOpacity', 0.8);
-					deliveryPoint.geometry.setCoordinates(coords);
-					deliveryPoint.options.set('iconColor', polygon.properties.get('fill'));
-					if (typeof(obj.getThoroughfare) === 'function') {
-						setData(obj);
-					} else {
-						ymaps.geocode(coords, {results: 1}).then(function (res) {
-							var obj = res.geoObjects.get(0);
-							setData(obj);
-						});
-					}
+			// Сохраняем координаты переданного объекта.
+			var coords = obj.geometry.getCoordinates(),
+			// Находим полигон, в который входят переданные координаты.
+				polygon = deliveryZones.searchContaining(coords).get(0);
+				
+			if (polygon) {
+				console.log(polygon.properties.get('description'));
+				// Уменьшаем прозрачность всех полигонов, кроме того, в который входят переданные координаты.
+				deliveryZones.setOptions('fillOpacity', 0.4);
+				polygon.options.set('fillOpacity', 0.8);
+				// Перемещаем метку с подписью в переданные координаты и перекрашиваем её в цвет полигона.
+				deliveryPoint.geometry.setCoordinates(coords);
+				deliveryPoint.options.set('iconColor', polygon.properties.get('fill'));
+				// Задаем подпись для метки.
+				if (typeof(obj.getThoroughfare) === 'function') {
+					setData(obj);
 				} else {
-					deliveryZones.setOptions('fillOpacity', 0.4);
-					deliveryPoint.geometry.setCoordinates(coords);
-					deliveryPoint.properties.set({
-						iconCaption: 'Доставка транспортной компанией',
-						balloonContent: 'Cвяжитесь с оператором',
-						balloonContentHeader: ''
-					});
-					deliveryPoint.options.set('iconColor', 'black');
-				}
-
-				function setData(obj) {
-					var address = [obj.getThoroughfare(), obj.getPremiseNumber(), obj.getPremise()].join(' ');
-					if (address.trim() === '') {
-						address = obj.getAddressLine();
-					}
-					var price = polygon.properties.get('description');
-					price = price.match(/<strong>(.+)<\/strong>/)[1];
-					deliveryPoint.properties.set({
-						iconCaption: address,
-						balloonContent: address,
-						balloonContentHeader: price
+					// Если вы не хотите, чтобы при каждом перемещении метки отправлялся запрос к геокодеру,
+					// закомментируйте код ниже.
+					ymaps.geocode(coords, {results: 1}).then(function (res) {
+						var obj = res.geoObjects.get(0);
+						setData(obj);
 					});
 				}
+			} else {
+				// Если переданные координаты не попадают в полигон, то задаём стандартную прозрачность полигонов.
+				deliveryZones.setOptions('fillOpacity', 0.4);
+				// Перемещаем метку по переданным координатам.
+				deliveryPoint.geometry.setCoordinates(coords);
+				// Задаём контент балуна и метки.
+				deliveryPoint.properties.set({
+					iconCaption: 'Доставка транспортной компанией',
+					balloonContent: 'Cвяжитесь с оператором',
+					balloonContentHeader: ''
+				});
+				// Перекрашиваем метку в чёрный цвет.
+				deliveryPoint.options.set('iconColor', 'black');
 			}
 
-			var sug;
-			$("#suggestions").blur(function() {
-				try {
-					if (!sug.data.house) {
-						tip_address.show();
-					}
-				} catch(e) {
-					tip_address.show();
+			function setData(obj){
+				var address = [obj.getThoroughfare(), obj.getPremiseNumber(), obj.getPremise()].join(' ');
+				if (address.trim() === '') {
+					address = obj.getAddressLine();
 				}
-			});
+				var price = polygon.properties.get('description');
+				price = price.match(/<strong>(.+)<\/strong>/)[1];
+				deliveryPoint.properties.set({
+					iconCaption: address,
+					balloonContent: address,
+					balloonContentHeader: price
+				});
+			}
+		}
+		
+		
+		var sug;
+		$("#suggestions").blur(function(){
+			console.log(sug);
+			
+			try{
+				if (!sug.data.house){
+					console.log("вывожу тип", tip_address);
+					tip_address.show();
+					
+					// $("#suggestions").prop("title","Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+					// alert("Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+				}
+			} catch(e) {
+				console.log("error",e );
+				// const tip_address = tippy(document.querySelector('#suggestions'));
+				console.log("вывожу тип", tip_address);
+				tip_address.show();
+				// $("#suggestions").prop("title","Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+				// alert("Пожалуйста, укажите адрес полностью: улица, дом, квартира");
 
-			$("#suggestions").suggestions({
-				token: "9e7eadbd4036f53c146fe2f69e3de359e78e9232",
-				type: "ADDRESS",
-				params: {
-					triggerSelectOnBlur: true,
-					triggerSelectOnEnter: true,
-					locations_geo: [{
+			}
+		});
+		$("#suggestions").suggestions({
+			token: "9e7eadbd4036f53c146fe2f69e3de359e78e9232",
+			type: "ADDRESS",
+			params: {
+				triggerSelectOnBlur: true,
+				triggerSelectOnEnter: true,
+				locations_geo: [
+					{
 						lat: 52.277419,
 						lon: 104.275385,
 						radius_meters: 65000
-					}]
-				},
-				onSelect: function(suggestion, changed) {
-					sug = suggestion;
-
-					if (!suggestion.data.house) {
-						tip_address.show();
 					}
+				]
+			},
+			// locations: [ "region", "Иркутская"],
+			// restrict_value: true,
+			/* Вызывается, когда пользователь выбирает одну из подсказок */
+		onSelect: function(suggestion, changed) {
+			console.log(suggestion);
+			sug=suggestion;
+			console.log(suggestion.data.house);
+			if (!suggestion.data.house){
+				console.log("вывожу тип", tip_address);
+				tip_address.show();
+				
+				// $("#suggestions").tooltip();
+			//     alert("Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+			}
+			console.log("коорды из дадата: ",[suggestion.data.geo_lat,suggestion.data.geo_lon]);
+			console.log("полигон: ",deliveryZones.searchContaining([suggestion.data.geo_lon, suggestion.data.geo_lat]).get(0));
+			var coords = [suggestion.data.geo_lon, suggestion.data.geo_lat],
+			// Находим полигон, в который входят переданные координаты.
+				polygon = deliveryZones.searchContaining(coords).get(0);
+				
+			if (polygon) {
+				console.log(polygon.properties.get('description'));
+				
+				
+				
+				//!
+				// $('<div class="t-text" name="dostavka_info"><br></div>').insertBefore("[data-input-lid='1606970309593'] > t-input-block");
+				
 
-					var lon = parseFloat(suggestion.data.geo_lon);
-					var lat = parseFloat(suggestion.data.geo_lat);
-
-					// Блокируем кнопку пока проверяем зону
-					$('button[type="submit"]').prop('disabled', true).css('opacity', '0.5');
-					$("[name='dostavka_info']").html('Проверяем адрес...');
-
-					checkZoneGeoJSON(lon, lat, function(zone) {
-
-						if (zone === 'сорокин') {
-							$("[name='dostavka_info']").html('<span style="color:green">✓ Адрес входит в зону доставки</span>');
-							$('button[type="submit"]').prop('disabled', false).css('opacity', '1');
-
-						} else if (zone === 'гусев') {
-							$("[name='dostavka_info']").html(
-								'<span style="color:red">Ваш адрес обслуживает другой филиал. ' +
-								'Оформите заказ на <a href="https://hongkong38.ru" target="_blank">hongkong38.ru</a></span>'
-							);
-							$('button[type="submit"]').prop('disabled', true).css('opacity', '0.5');
-
-						} else {
-							$("[name='dostavka_info']").html(
-								'<span style="color:red">Ваш адрес не входит в зону доставки. ' +
-								'Позвоните нам: <a href="tel:+73952959797">95-97-97</a></span>'
-							);
-							$('button[type="submit"]').prop('disabled', true).css('opacity', '0.5');
-						}
+				// $("[data-input-lid='1606970309593']").append('<div class="t-text" name="dostavka_info"><br></div>');
+				$("[name='dostavka_info'").html(polygon.properties.get('description'));
+				//!
+				
+				
+				
+				
+				
+				// Уменьшаем прозрачность всех полигонов, кроме того, в который входят переданные координаты.
+				deliveryZones.setOptions('fillOpacity', 0.4);
+				polygon.options.set('fillOpacity', 0.8);
+				// Перемещаем метку с подписью в переданные координаты и перекрашиваем её в цвет полигона.
+				deliveryPoint.geometry.setCoordinates(coords);
+				deliveryPoint.options.set('iconColor', polygon.properties.get('fill'));
+				// Размещаем текст в поиске карты из инпута в корзине
+				$(".ymaps-2-1-77-searchbox-input__input").val($("#suggestions").val());
+				// Задаем подпись для метки.
+				if (typeof(obj.getThoroughfare) === 'function') {
+					setData(obj);
+				} else {
+					// Если вы не хотите, чтобы при каждом перемещении метки отправлялся запрос к геокодеру,
+					// закомментируйте код ниже.
+					ymaps.geocode(coords, {results: 1}).then(function (res) {
+						var obj = res.geoObjects.get(0);
+						setData(obj);
 					});
-
-					// Обновляем карту
-					var coords = [lon, lat];
-					var polygon = deliveryZones.searchContaining(coords).get(0);
-					if (polygon) {
-						deliveryZones.setOptions('fillOpacity', 0.4);
-						polygon.options.set('fillOpacity', 0.8);
-						deliveryPoint.geometry.setCoordinates(coords);
-						deliveryPoint.options.set('iconColor', polygon.properties.get('fill'));
-						$(".ymaps-2-1-77-searchbox-input__input").val($("#suggestions").val());
-					} else {
-						$("[name='dostavka_info']").html(
-							'<span style="color:red">Ваш адрес не входит в зону доставки. ' +
-							'Позвоните нам: <a href="tel:+73952959797">95-97-97</a></span>'
-						);
-					}
-				},
-				onSuggestionsFetch: function(suggestion) {
-					console.log("подсказки загрузились");
-				},
-				onSelectNothing: function(suggestion) {
-					try {
-						if (!suggestion.data.house) {
-							tip_address.show();
-						}
-					} catch(e) {
-						tip_address.show();
-					}
 				}
-			});
-		}
+			} else {
+				
+				// $("#suggestions").prop("title","Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+				// $("#suggestions").tooltip();
+				$("[name='dostavka_info'").html("К сожалению, ваш адрес не входит в зоны нашей доставки. Но, мы обязательно что-нибудь придумаем! Просто позвоните нам по тел <a href='tel:+73952746486' style=''>746-486</a>");
 
-		$.getJSON('https://morda-boy.github.io/gk-delievery-sorokin-04-2026.geojson', function(data) {
-			console.log(data, obj);
-			obj = data;
-			onZonesLoad(obj);
+			}
+			// if (suggestion.data.house)
+		},
+		// onSelect: function(changed, suggestion) {
+		//     console.log("Произошли изменения");
+		//     if
+			
+		// },
+		onSuggestionsFetch: function(suggestion){
+			console.log("подсказки загрузились");
+			console.log(suggestion);
+			// setSuggestion(suggestion);
+		},
+		onSelectNothing: function(suggestion) {
+			console.log("Фокус без выбранной подсказки");
+			console.log(suggestion);
+			
+			
+			try{
+				if (!suggestion.data.house){
+					console.log("вывожу тип", tip_address);
+					tip_address.show();
+					
+					// alert("Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+				}
+			} catch(e) {
+				console.log("error",e );
+				console.log("вывожу тип", tip_address);
+				tip_address.show();
+				// alert("Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+
+			}
+			
+			// alert("Пожалуйста, укажите адрес полностью: улица, дом, квартира");
+		}
 		});
+		
 	}
+		
+
+
+	$.getJSON('https://morda-boy.github.io/map-hongkong-2025-02.geojson', function(data) {
+		
+	  console.log(data, obj);
+	  obj=data;
+	  // obj=jQuery.parseJSON(data);
+	  onZonesLoad(obj);
+	});
+			//  alert( obj.name);
+	/* $.getJSON(url, function (data) {
+		alert(data.name);
+		onZonesLoad(data);
+	}); */
+	/* var json = jQuery.parseJSON() */
+	/* console.log(json) */
+	/* onZonesLoad(json) */
+}
 });
