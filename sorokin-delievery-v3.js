e7f058a3bff4bae18d1543a3b8e9072260d33cad
Morda-boy <34206382+Morda-boy@users.noreply.github.com>
2026-04-14 18:38:45 +0800

    Update sorokin-delievery-v3.js

--- sorokin-delievery-v3.js
+++ sorokin-delievery-v3.js
@@ -4,8 +4,6 @@ $(window).load(function() {
 	$("[name='adress']").attr('id', 'suggestions');
 	// $("[name='adress']").attr('data-msg', 'Пожалуйста, укажите адрес полностью: улица, дом, квартира');
 $('<div class="t-text" name="dostavka_info" style="margin-top: 10px;font-style: italic;"><br></div>').insertAfter("[name='adress']");
-// $("#suggestions").attr("title","Пожалуйста, укажите адрес полностью: улица, дом, квартира");
-// $("#suggestions").attr("data-toggle","tooltip");
 tippy('#suggestions', {theme: 'custom-dark', content: 'Пожалуйста, укажите адрес полностью: улица, дом, квартира',});
 const tip_address = tippy(document.querySelector('#suggestions'));;
 tip_address.setContent('Пожалуйста, укажите адрес полностью: улица, дом, квартира');
@@ -22,7 +20,6 @@ var myMap;
 var deliveryZones;
 var obj;
 
-	// *** НОВОЕ: проверка попадания точки в полигон ***
 	function pointInPoly(point, polygon) {
 		var x = point[0], y = point[1], inside = false;
 		for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
@@ -35,9 +32,8 @@ var obj;
 		return inside;
 	}
 
-	// *** НОВОЕ: проверка попадания в любую зону из GeoJSON ***
 	function isInAnyZone(lon, lat, callback) {
-		fetch('https://morda-boy.github.io/gk138-04-04-2026.geojson')
+		fetch('https://morda-boy.github.io/gk-delievery-sorokin-04-2026.geojson')
 			.then(function(r) { return r.json(); })
 			.then(function(data) {
 				var found = false;
@@ -78,9 +74,7 @@ function init() {
 	myMap.geoObjects.add(deliveryPoint);
 
 	function onZonesLoad(json) {
-		// Добавляем зоны на карту.
 		deliveryZones = ymaps.geoQuery(json).addToMap(myMap);
-		// Задаём цвет и контент балунов полигонов.
 		deliveryZones.each(function (obj) {
 			obj.options.set({
 				fillColor: obj.properties.get('fill'),
@@ -92,32 +86,28 @@ function init() {
 			obj.properties.set('balloonContent', obj.properties.get('description'));
 		});
 
-		// *** НОВОЕ: при смене варианта доставки — разблокируем кнопку ***
+		// При смене варианта доставки — разблокируем кнопку
 		$('input[name="deliveryvar"]').on('change', function() {
 			if ($(this).val() !== 'Доставка (до подъезда)') {
-				$('button[type="submit"]').prop('disabled', false).css('opacity', '1');
+				$('.t-submit').prop('disabled', false).css('opacity', '1').css('pointer-events', 'auto');
 				$("[name='dostavka_info']").html('');
 			}
 		});
 
-		// Проверим попадание результата поиска в одну из зон доставки.
 		searchControl.events.add('resultshow', function (e) {
 			console.log("еее  бой");
 			highlightResult(searchControl.getResultsArray()[e.get('index')]);
 		});
 
-		// Проверим попадание метки геолокации в одну из зон доставки.
 		myMap.controls.get('geolocationControl').events.add('locationchange', function (e) {
 			highlightResult(e.get('geoObjects').get(0));
 		});
 
-		// При перемещении метки сбрасываем подпись, содержимое балуна и перекрашиваем метку.
 		deliveryPoint.events.add('dragstart', function () {
 			deliveryPoint.properties.set({iconCaption: '', balloonContent: ''});
 			deliveryPoint.options.set('iconColor', 'black');
 		});
 
-		// По окончании перемещения метки вызываем функцию выделения зоны доставки.
 		deliveryPoint.events.add('dragend', function () {
 			highlightResult(deliveryPoint);
 		});
@@ -127,21 +117,16 @@ function init() {
 			console.log(obj.geometry.getCoordinates());
 			$("#suggestions").val($(".ymaps-2-1-77-searchbox-input__input").val());
 
-			// Сохраняем координаты переданного объекта.
 			var coords = obj.geometry.getCoordinates(),
-			// Находим полигон, в который входят переданные координаты.
 				polygon = deliveryZones.searchContaining(coords).get(0);
 
 			if (polygon) {
 				console.log(polygon.properties.get('description'));
 				$("[name='dostavka_info'").html(polygon.properties.get('description'));
-				// Уменьшаем прозрачность всех полигонов, кроме того, в который входят переданные координаты.
 				deliveryZones.setOptions('fillOpacity', 0.4);
 				polygon.options.set('fillOpacity', 0.8);
-				// Перемещаем метку с подписью в переданные координаты и перекрашиваем её в цвет полигона.
 				deliveryPoint.geometry.setCoordinates(coords);
 				deliveryPoint.options.set('iconColor', polygon.properties.get('fill'));
-				// Задаем подпись для метки.
 				if (typeof(obj.getThoroughfare) === 'function') {
 					setData(obj);
 				} else {
@@ -151,17 +136,13 @@ function init() {
 					});
 				}
 			} else {
-				// Если переданные координаты не попадают в полигон, то задаём стандартную прозрачность полигонов.
 				deliveryZones.setOptions('fillOpacity', 0.4);
-				// Перемещаем метку по переданным координатам.
 				deliveryPoint.geometry.setCoordinates(coords);
-				// Задаём контент балуна и метки.
 				deliveryPoint.properties.set({
 					iconCaption: 'Доставка транспортной компанией',
 					balloonContent: 'Cвяжитесь с оператором',
 					balloonContentHeader: ''
 				});
-				// Перекрашиваем метку в чёрный цвет.
 				deliveryPoint.options.set('iconColor', 'black');
 			}
 
@@ -180,11 +161,9 @@ function init() {
 			}
 		}
 		
-		
 		var sug;
 		$("#suggestions").blur(function(){
 			console.log(sug);
-			
 			try{
 				if (!sug.data.house){
 					console.log("вывожу тип", tip_address);
@@ -196,6 +175,7 @@ function init() {
 				tip_address.show();
 			}
 		});
+
 		$("#suggestions").suggestions({
 			token: "9e7eadbd4036f53c146fe2f69e3de359e78e9232",
 			type: "ADDRESS",
@@ -240,23 +220,23 @@ function init() {
 					});
 				}
 			} else {
-				$("[name='dostavka_info'").html("К сожалению, ваш адрес не входит в зоны нашей доставки. Но, мы обязательно что-нибудь придумаем! Просто позвоните нам по тел <a href='tel:+73952746486' style=''>95-97-97</a>");
+				$("[name='dostavka_info'").html("К сожалению, ваш адрес не входит в зоны нашей доставки. Но, мы обязательно что-нибудь придумаем! Просто позвоните нам по тел <a href='tel:+73952746486' style=''>959797</a>");
 			}
 
-			// *** НОВОЕ: дополнительно проверяем по нашему GeoJSON и блокируем кнопку ***
+			// Проверяем по GeoJSON и блокируем кнопку
 			var lon = parseFloat(suggestion.data.geo_lon);
 			var lat = parseFloat(suggestion.data.geo_lat);
-			$('button[type="submit"]').prop('disabled', true).css('opacity', '0.5');
+			$('.t-submit').prop('disabled', true).css('opacity', '0.5').css('pointer-events', 'none');
 
 			isInAnyZone(lon, lat, function(inZone) {
 				if (inZone) {
-					$('button[type="submit"]').prop('disabled', false).css('opacity', '1');
+					$('.t-submit').prop('disabled', false).css('opacity', '1').css('pointer-events', 'auto');
 				} else {
 					$("[name='dostavka_info']").html(
 						'<span style="color:red">Ваш адрес не входит в зону доставки. ' +
 						'Позвоните нам: <a href="tel:+73952959797">95-97-97</a></span>'
 					);
-					$('button[type="submit"]').prop('disabled', true).css('opacity', '0.5');
+					$('.t-submit').prop('disabled', true).css('opacity', '0.5').css('pointer-events', 'none');
 				}
 			});
 		},
@@ -267,7 +247,6 @@ function init() {
 		onSelectNothing: function(suggestion) {
 			console.log("Фокус без выбранной подсказки");
 			console.log(suggestion);
-			
 			try{
 				if (!suggestion.data.house){
 					console.log("вывожу тип", tip_address);
@@ -283,7 +262,7 @@ function init() {
 		
 	}
 		
-	$.getJSON('https://morda-boy.github.io/gk-delievery-sorokin-04-2026.geojson', function(data) {
+	$.getJSON('https://morda-boy.github.io/gk-delievery-sorokin-04-2026.geojsonn', function(data) {
 		console.log(data, obj);
 		obj=data;
 		onZonesLoad(obj);
